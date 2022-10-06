6fa61feb0715cbd78c4972e11a16737c1d1f8c8a
Linh777GKOG <97266612+Linh777GKOG@users.noreply.github.com>
2022-10-06 13:57:16 +0700

    commit msg

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.css';
+import '../styles/global.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
 
